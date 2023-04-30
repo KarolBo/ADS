7e63b1a8277630b5dd390a7319c5e399b6c9d760
Karol Borkowski <karol.borkowski@gb-rayz.ch>
2023-04-30 20:39:13 +0200

    grahams - sorting based on orientation

--- convex_hull.py
+++ convex_hull.py
@@ -1,9 +1,10 @@
+from functools import cmp_to_key
+
 def jarvis(points: list) -> list:
     points = [(x, y) for x, y in points]
     p0 = find_bottom_most(points)
     hull = []
     points_set = set(points)
-    n = len(points)
 
     p = p0
     while True:
@@ -20,6 +21,17 @@ def jarvis(points: list) -> list:
 
 ###########################################################
 
+def graham_scan(points: list) -> list:
+    points = [(x, y) for x, y in points]
+    p0 = find_bottom_most(points)
+    points.remove(p0)
+    hull = [p0]
+    points.sort(key=cmp_to_key(lambda p, q: orientation_cmp(p0, p, q)))
+    print(points)
+
+
+###########################################################
+
 def find_bottom_most(points: list) -> list:
     bm = points[0]
     for p in points[1:]:
@@ -33,9 +45,21 @@ def get_orientation(p1, p2, p3):
     return (p2[0] - p1[0]) * (p3[1] - p2[1]) - (p2[1] - p1[1]) * (p3[0] - p2[0])
 
 
+def orientation_cmp(p0, p, q):
+    orientation = get_orientation(p0, p, q)
+    if orientation < 0:
+        return 1
+    if orientation > 0:
+        return -1
+    if p[0]*p[0] + p[0]*p[1] < q[0]*q[0] + q[0]*q[1]:
+        return -1
+    return 1
+
+
 ###########################################################
 
 test_input = [[1,1],[2,2],[2,0],[2,4],[3,3],[4,2]]
 test_output = [[1,1],[2,0],[4,2],[3,3],[2,4]]
-output = jarvis(test_input)
-print(output.sort() == test_output.sort())
+# output = jarvis(test_input)
+# print(output.sort() == test_output.sort())
+graham_scan(test_input)
